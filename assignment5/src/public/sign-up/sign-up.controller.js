1a5920b8cbd68e7cd7849ce10291a15caf83f69b
Thomas MARTIN <tmartin@live.fr>
2018-08-13 00:53:49 +0200

    remove useless get to distant server when submit & minor fixes

--- assignment5/src/public/sign-up/sign-up.controller.js
+++ assignment5/src/public/sign-up/sign-up.controller.js
@@ -14,22 +14,8 @@ function SignUpController(UserService, $scope) {
   }
 
   reg.submit = function () {
-  	if (reg.user.short_name == "" || !reg.user.short_name) {
   		UserService.setUserData(reg.user);
     	reg.completed = true;
-  	} 
-  	else {
-    	var promise = UserService.getFavoriteItem(reg.user.short_name);
-    	promise.then(function(response) {
-    		reg.user.favoriteItem = response;
-    		UserService.setUserData(reg.user);
-    		reg.completed = true;
-        reg.badDish = false;
-    	})
-    	.catch(function (error) {
-    		reg.badDish = true;
-    	});		
-    }
   };
   
   $scope.$watch($scope.short_name, function(newValue, oldValue){
